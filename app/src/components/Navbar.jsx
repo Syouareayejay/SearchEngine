ed12d55b755ae466a154b97c6cd7df2eb2198d30
Suraj Singh <jatarogiovani@gmail.com>
2021-11-11 15:50:01 +0530

    navbar v1

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export const Navbar = ({setDarktheme,darkTheme}) => {
+export const Navbar = ({setDarkTheme,darkTheme}) => {
     return (
-        <div>
-            navbar
-        </div>
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+    <div className="flex justify-between items-center space-x-5 w-screen ">
+      <Link to="/">
+          searchEngine 🔎
+      </Link>
+    </div>
+  </div>
     )
 }
 
